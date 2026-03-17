daf28741f381df2d292bdc2c2064ad9f95825144
CMP7005 <prasad.amrita34@gmail.com>
2026-03-17 23:49:18 +0000

    test run

---
+++ air_quality_app/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Air Quality App")
+
+st.title("Air Quality Analytics App")
+
+st.write("""
+This application contains three sections:
+
+1️⃣ Upload Dataset
+2️⃣ Data Description and Preprocessing
+3️⃣ Exploratory Data Analysis (EDA)
+4️⃣ Model Building
+""")
+
+st.info("Use the sidebar to navigate between pages.")
