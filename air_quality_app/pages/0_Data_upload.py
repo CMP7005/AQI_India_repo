3046efb178646d9e197c2fbfee52cc3fbb37221a
CMP7005 <prasad.amrita34@gmail.com>
2026-03-18 12:25:47 +0000

    Update 0_Data_upload.py-modified

--- air_quality_app/pages/0_Data_upload.py
+++ air_quality_app/pages/0_Data_upload.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-url = "https://raw.githubusercontent.com/USERNAME/REPO/main/air_quality.csv"
+url = "https://raw.githubusercontent.com/CMP7005/AQI_Indian_repo/main/city_day(2).csv"
 df = pd.read_csv(url)
